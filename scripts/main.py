f6869326c311478acb9949fea882040afc162407
Ayazdi <amirali.yazdi@yahoo.com>
2020-04-11 17:52:34 +0200

    add money spent for each aisle

--- scripts/main.py
+++ scripts/main.py
@@ -4,42 +4,56 @@ import cv2
 from config import PATH
 
 
+if __name__ == '__main__':
 
-if __name__=='__main__':
-
-    customers = []
+    CUSTOMERS = []
     for _ in range(5):
-        c = Customer(weekday='Friday', image=cv2.imread(f'{PATH}customer_2.png'))
-        c.pattern()
-        c.money_spent()
-        customers.append(c)
+        C = Customer(weekday='Friday')
+        C.pattern()
+        C.money_spent()
+        CUSTOMERS.append(C)
 
-    for customer in customers:
+    for customer in CUSTOMERS:
         print(customer.shopping_hist)
         print(customer.payment)
+        # print(customer.money_per_aisle)
 
-    sim = SupermarketSim(customers)
-    img = cv2.imread(f'{PATH}market.png')
+    SIM = SupermarketSim(CUSTOMERS)
+    IMG = cv2.imread(f'{PATH}market.png')
 
-    income = []
+    AISLE_INCOME = {'fruit': 0, 'spices': 0, 'drinks': 0, 'dairy': 0}
+    INCOME = []
     while True:
 
-        frame = img.copy()
-        layer = np.zeros((sim.frame.shape[0], sim.frame.shape[1], 3))
-        sim.draw()
-        sim.run_one_iteration()
+        frame = IMG.copy()
+        layer = np.zeros((SIM.frame.shape[0], SIM.frame.shape[1], 3))
+        SIM.draw()
+        SIM.run_one_iteration()
 
-        for customer in sim.customers:
+        for customer in SIM.customers:
             if customer.location[0] == customer.target_position[0] and customer.location[1] == customer.target_position[1]:
-                income.append(customer.payment)
-                sim.customers.remove(customer)
+                INCOME.append(customer.payment)
+                SIM.customers.remove(customer)
 
-        cv2.putText(layer, f'{int(sum(income))}', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (1, 1, 1), 2)
+        for customer in SIM.customers:
+            for key in AISLE_INCOME.keys():
+                if customer.location[0] == customer.aisles_locs[key][0] and customer.location[1] == customer.aisles_locs[key][1]:
+                    AISLE_INCOME[key] += customer.money_per_aisle[key]
+                    print(AISLE_INCOME)
+
+
+
+        cv2.putText(layer, f"{int(AISLE_INCOME['fruit'])}", (650, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (1, 1, 1), 2)
+        cv2.putText(layer, f"{int(AISLE_INCOME['spices'])}", (450, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (1, 1, 1), 2)
+        cv2.putText(layer, f"{int(AISLE_INCOME['drinks'])}", (350, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (1, 1, 1), 2)
+        cv2.putText(layer, f"{int(AISLE_INCOME['dairy'])}", (200, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (1, 1, 1), 2)
+
+        cv2.putText(layer, f'{int(sum(INCOME))}', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (1, 1, 1), 2)
 
         cnd = layer[:] > 0
-        sim.frame[cnd] = layer[cnd]
+        SIM.frame[cnd] = layer[cnd]
 
-        cv2.imshow('frame', sim.frame)
+        cv2.imshow('frame', SIM.frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
